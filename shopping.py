94be8204d63504dc201191e8a85f8502641ba497
MarcoValenteF <mafigueira@deloitte.pt>
2025-11-18 12:18:11 +0000

    Add SVM model to the shopping predictor

--- shopping.py
+++ shopping.py
@@ -52,7 +52,8 @@ def main():
         # Train the K-NN model on training data and make predictions on test data
         "KNN": KNeighborsClassifier(n_neighbors=3),
         "Random Forest": RandomForestClassifier(n_estimators=100, random_state=42),
-        "Logistic Regression": LogisticRegression(max_iter=5000, solver="saga")
+        "Logistic Regression": LogisticRegression(max_iter=5000, solver="saga"),
+        "SVM": SVC(kernel="linear")
     }
 
     # Tuning for Random Forest
